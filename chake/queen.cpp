ed270943d5547c607b55446a87aeb8a3053a4a79
ruth-ioffe <ruth.ioffe.study@gmail.com>
2025-01-12 22:14:18 +0200

    fixed queen

--- chake/queen.cpp
+++ chake/queen.cpp
@@ -1,12 +1,10 @@
 #include "queen.h"
-#include "bishop.h"
-#include "rook.h"
 
 queen::queen(char color) : piece()
 {
-	this->color = color;
-	this->check = false;
-	this->set_type();
+    this->color = color;
+    this->check = false;
+    this->set_type();
 }
 
 queen::~queen()
@@ -16,17 +14,17 @@ queen::~queen()
 
 bool queen::movement(int move_from, int move_to, piece* (&board)[8][8])
 {
-	int row_from = move_from / 10;
-	int col_from = move_from % 10;
-	int row_to = move_to / 10;
-	int col_to = move_to % 10;
+    int row_from = move_from / 10;
+    int col_from = move_from % 10;
+    int row_to = move_to / 10;
+    int col_to = move_to % 10;
 
-	if (bishopMove(move_from, move_to, board) || rookMove(move_from, move_to, board))
-	{
-		return true; 
-	}
+    if (bishopMove(move_from, move_to, board) || rookMove(move_from, move_to, board))
+    {
+        return true;
+    }
 
-	return false; 
+    return false;
 }
 
 void queen::set_check()
@@ -35,26 +33,26 @@ void queen::set_check()
 }
 bool queen::get_check()
 {
-	return this->check;
+    return this->check;
 }
 char queen::get_color() const
 {
-	return this->color;
+    return this->color;
 }
 void queen::set_type()
 {
-	if (this->get_color() == 'w')
-	{
-		this->type = 'Q';
-	}
-	else if (this->get_color() == 'b')
-	{
-		this->type = 'q';
-	}
+    if (this->get_color() == 'w')
+    {
+        this->type = 'Q';
+    }
+    else if (this->get_color() == 'b')
+    {
+        this->type = 'q';
+    }
 }
 char queen::get_type() const
 {
-	return this->type;
+    return this->type;
 }
 
 bool queen::rookMove(int move_from, int move_to, piece* (&board)[8][8])
@@ -69,32 +67,82 @@ bool queen::rookMove(int move_from, int move_to, piece* (&board)[8][8])
     int row_diff = row_to - row_from;
     int col_diff = col_to - col_from;
 
+    int i = 0;
+    int step = 0;
+
 
 
     if ((row_from != row_to && col_from == col_to) || (row_from == row_to && col_from != col_to)) // if move in a lein
     {
 
+        if (row_diff < 0)
+        {
+
+            step = -1;
+
+        }
+        else
+        {
+
+            step = 1;
+
+        }
+
         //chake move
         if (row_from != row_to && col_from == col_to)
         {
             //col is fixed == false
-            if (cal_move(row_diff, row_from, row_to, false, board))
+            i = row_from;
+            while ((i += step) != row_to)
             {
 
-                board[row_to][col_to] = board[row_from][col_from];
-                board[row_from][col_from] = nullptr;
+                if (board[i][col_from] != nullptr)
+                {
+                    return false;
+
+                }
+
+            }
+
+            if (board[row_to][col_to] != nullptr)
+            {
+
+                return eat(col_from, row_from, col_to, row_to, board);
+
+            }
+            else
+            {
+
+                move(col_from, row_from, col_to, row_to, board);
                 return true;
 
             }
 
         }
         else
-        {											//row is fixed == true
-            if (cal_move(col_diff, col_from, col_to, true, board))
+        {
+            i = col_from;
+            while ((i += step) != col_to)
             {
 
-                board[row_to][col_to] = board[row_from][col_from];
-                board[row_from][col_from] = nullptr;
+                if (board[row_from][i] != nullptr)
+                {
+                    return false;
+
+                }
+
+            }
+
+            if (board[row_to][col_to] != nullptr)
+            {
+
+                return eat(col_from, row_from, col_to, row_to, board);
+
+            }
+            else
+            {
+
+                move(col_from, row_from, col_to, row_to, board);
                 return true;
 
             }
@@ -106,89 +154,30 @@ bool queen::rookMove(int move_from, int move_to, piece* (&board)[8][8])
     return false;
 }
 
-bool queen::cal_move(int diff, int from, int to, bool fixed, piece* (&board)[8][8])
+bool queen::eat(int col_from, int row_from, int col_to, int row_to, piece* (&board)[8][8])
 {
-    int i = from;
-    int step = 0;
 
-    if (diff < 0)
+    if (board[row_to][col_to]->get_color() == this->color)
     {
-
-        step = -1;
-
+        return false;
     }
     else
     {
 
-        step = 1;
-
-    }
-
-    if (fixed)
-    {
-
-        while ((i += step) != to)
-        {
-
-            if (board[fixed][i] != nullptr)
-            {
-
-                return false;
-
-            }
-
-        }
-
-        if (board[fixed][to] != nullptr)
-        {
-            if (board[fixed][to]->get_color() == this->color)
-            {
-                return false;
-            }
-            else
-            {
-                delete board[fixed][to];
-                printf("\nkilled!\n");
-                board[fixed][to] = nullptr;
-
-            }
-        }
-
-    }
-    else
-    {
-
-        while ((i += step) != to)
-        {
-
-            if (board[i][fixed] != nullptr)
-            {
-
-                return false;
-
-            }
-
-        }
-
-        if (board[to][fixed] != nullptr)
-        {
-            if (board[to][fixed]->get_color() == this->color)
-            {
-                return false; // Cannot move to a square occupied by your own piece
-            }
-            else
-            {
-                delete board[to][fixed]; // Capture and delete the opponent's piece
-                printf("\nkilled!\n");
-                board[to][fixed] = nullptr; // Clear the destination square
-
-            }
-        }
-
+        move(col_from, row_from, col_to, row_to, board);
     }
 
     return true;
 }
+void queen::move(int col_from, int row_from, int col_to, int row_to, piece* (&board)[8][8])
+{
+    delete board[row_to][col_to];
+    printf("\nkilled!\n");
+    board[row_to][col_to] = board[row_from][col_from];
+    board[row_from][col_from] = nullptr;
+}
+
+
 bool queen::bishopMove(int move_from, int move_to, piece* (&board)[8][8])
 {
     int row_from = move_from / 10;
--- chake/queen.h
+++ chake/queen.h
@@ -14,10 +14,11 @@ public:
 	void set_type() override;
 	char get_type() const override;
 	bool rookMove(int move_from, int move_to, piece* (&board)[8][8]);
-	bool cal_move(int diff, int from, int to, bool fixed, piece* (&board)[8][8]);
+
 	bool bishopMove(int move_from, int move_to, piece* (&board)[8][8]);
-	
-	
+	bool eat(int col_from, int row_from, int col_to, int row_to, piece* (&board)[8][8]);
+	void move(int col_from, int row_from, int col_to, int row_to, piece* (&board)[8][8]);
+
 
 private:
 	char color;
